bdc96fec1f1862bf4e65ce4d31b146c841533517
Krista Calderon <kamsci@gmail.com>
2017-06-10 11:53:24 -0700

    Created constants for actionTypes

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,7 @@
+import * as types from './actionTypes';
+
 export function createCourse(course) {
   // debugger;
-  return { type: 'CREATE_COURSE', course }
+  return { type: types.CREATE_COURSE, course }
   /* 'course' is same as 'course: course' */
 }
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,7 +1,9 @@
+import * as types from '../actions/actionTypes';
+
 /* Initialize state to empty array - no courses, used as initial state in store */
 export default function courseReducer(state = [], action) {
   switch (action.type) {
-    case 'CREATE_COURSE':
+    case types.CREATE_COURSE:
       // debugger;
       /* ES6 spread operator 
       ...state => explodes into a new instance of our state array
